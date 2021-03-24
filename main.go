3e96000249d1c21c87cdb25e968e13127b6b428a
gleb.bes@gmail.com <gabeskhlebnyy@avito.ru>
2021-03-24 15:37:24 +0300

    update numbers

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"github.com/shopspring/decimal"
-
 	testlib "github.com/GlebAltshifter/go-mod-versioning-test-lib/v2"
+	"github.com/shopspring/decimal"
 )
 
 func main() {
-	print(testlib.Sum(decimal.New(10, 1), decimal.New(11, 1)).String())
+	print(testlib.Sum(decimal.New(12, 1), decimal.New(11, 1)).String())
 }
